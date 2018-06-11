0f55abd8ac1e471c01058e33bb4f5126531ce3a6
Max Veasey <mveasey87@gmail.com>
2018-06-11 19:23:38 +0100

    refactored _diagonal_movement function

--- src/game_helper.py
+++ src/game_helper.py
@@ -1,9 +1,13 @@
 """Module of helper functions for chess game.
 
-   TODO Document functions.
+   Functions:
+        move_direction: Return str representation of move direction.
 """
 def move_direction(piece, to_coords):
-    """Calculate direction from piece to coordinates. Return str."""
+    """Calculate direction from piece to coordinates. Return str.
+       
+       Return values: 'diagonal', 'horizontal', 'vertical', 'non_linear'
+    """
     if _diagonal_movement(piece, to_coords):
         direction = 'diagonal'
     elif piece.x_coord != to_coords.x and piece.y_coord == to_coords.y:
@@ -17,11 +21,7 @@ def move_direction(piece, to_coords):
 
 def _diagonal_movement(piece, coords):
     """Helper function fro move_direction. Return bool."""
-    # TODO Last minute function re-write to fix bug. Can it be improved?
-    min_x_coord = min(piece.x_coord, coords.x)
-    min_y_coord = min(piece.y_coord, coords.y)
-    max_x_coord = max(piece.x_coord, coords.x)
-    max_y_coord = max(piece.y_coord, coords.y)
-    x_coord_range = list(range(min_x_coord, max_x_coord))
-    y_coord_range = list(range(min_y_coord, max_y_coord))
-    return len(x_coord_range) == len(y_coord_range)
+    min_x_coord, max_x_coord = sorted([piece.x_coord, coords.x])
+    min_y_coord, max_y_coord = sorted([piece.y_coord, coords.y])
+    # Only diagonal if distance equal lengths
+    return (max_x_coord - min_x_coord) == (max_y_coord - min_y_coord)
